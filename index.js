7cf7672545baab95d1008a1f78847424399b8ec2
bainchild <55368789+bainchild@users.noreply.github.com>
2021-01-13 21:51:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const WebSocket = require('ws');
-const WebSocket = require('ws');
 const server = new WebSocket.Server({
   port: 1234
 });
